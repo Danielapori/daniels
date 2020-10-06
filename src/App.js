d2af6a447ba46a1e524537e2490929be402be4cc
daniels <aporidaniel235@gmail.com>
2020-10-06 03:27:25 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import FirstComment from './FirstComment'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <FirstComment />
+      <p>This is a new world of preMEST </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FirstComment.js
@@ -0,0 +1,12 @@
+import react from 'react'
+function FirstComment () {
+  return (
+    <>
+      <p>This is our first component</p>
+      <p>Another Paragraph</p>
+    </>
+    
+  )
+}
+
+export default FirstComment;
